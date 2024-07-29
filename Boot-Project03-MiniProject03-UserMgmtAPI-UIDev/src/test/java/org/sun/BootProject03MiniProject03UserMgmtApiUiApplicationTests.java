b815d9a7939d3fb2f6f5b0e38338750f7b4ceac4
Rinku <nayaksunilkumar903@gmail.com>
2024-07-29 14:29:41 +0530

    springboot-docker commit

---
+++ Boot-Project03-MiniProject03-UserMgmtAPI-UIDev/src/test/java/org/sun/BootProject03MiniProject03UserMgmtApiUiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProject03MiniProject03UserMgmtApiUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
