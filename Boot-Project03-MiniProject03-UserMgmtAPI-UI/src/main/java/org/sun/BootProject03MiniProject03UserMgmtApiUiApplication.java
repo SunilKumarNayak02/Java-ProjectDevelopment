9dce7de95cce4e199654ebcb83a5e419881bde4f
Rinku <nayaksunilkumar903@gmail.com>
2024-07-29 14:55:20 +0530

    spring-docker commit

---
+++ Boot-Project03-MiniProject03-UserMgmtAPI-UI/src/main/java/org/sun/BootProject03MiniProject03UserMgmtApiUiApplication.java
@@ -0,0 +1,13 @@
+package org.sun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProject03MiniProject03UserMgmtApiUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProject03MiniProject03UserMgmtApiUiApplication.class, args);
+	}
+
+}
